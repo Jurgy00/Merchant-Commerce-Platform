ac2a2eee4a13a94dc28b7583525939f1e9a0b916
Jurgy00 <99367489+Jurgy00@users.noreply.github.com>
2026-07-24 19:19:31 +0000

    Initialize Spring Boot project skeleton

---
+++ backend/src/main/java/com/jurgens/merchantplatform/MerchantplatformApplication.java
@@ -0,0 +1,13 @@
+package com.jurgens.merchantplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerchantplatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MerchantplatformApplication.class, args);
+    }
+
+}
