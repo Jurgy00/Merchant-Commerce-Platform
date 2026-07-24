f1e2c9e2e7e9c94b612e6255604a2a966cf4f395
Jurgy00 <99367489+Jurgy00@users.noreply.github.com>
2026-07-24 19:29:56 +0000

    Add Category and Product entities

---
+++ backend/src/main/java/com/jurgens/merchantplatform/entities/Category.java
@@ -0,0 +1,35 @@
+package com.jurgens.merchantplatform.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "merchant_id", nullable = false)
+    private Merchant merchant;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private List<Product> products = new ArrayList<>();
+}
