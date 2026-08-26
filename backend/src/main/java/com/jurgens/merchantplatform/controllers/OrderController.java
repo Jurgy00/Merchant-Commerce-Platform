62fd8f5723d278080959a1a2b5aaea47e79c1056
jurgy00 <jurgenskips@gmail.com>
2026-08-26 18:15:55 +0300

    Create odercontroller

---
+++ backend/src/main/java/com/jurgens/merchantplatform/controllers/OrderController.java
@@ -0,0 +1,36 @@
+package com.jurgens.merchantplatform.controllers;
+
+import com.jurgens.merchantplatform.entities.Order;
+import com.jurgens.merchantplatform.services.OrderService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/orders")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Order> createOrder(@RequestBody Order order) {
+        Order createdOrder = orderService.createOrder(order);
+        return new ResponseEntity<>(createdOrder, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Order>> getAllOrders() {
+        return ResponseEntity.ok(orderService.getAllOrders());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Order> getOrderById(@PathVariable Long id) {
+        return ResponseEntity.ok(orderService.getOrderById(id));
+    }
+}
