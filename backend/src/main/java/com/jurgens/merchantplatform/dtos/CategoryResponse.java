1d921f1e6f756b47271f8f4ca173b874bac76ba4
jurgy00 <jurgenskips@gmail.com>
2026-09-23 13:44:35 +0300

    Add DTOs for category

---
+++ backend/src/main/java/com/jurgens/merchantplatform/dtos/CategoryResponse.java
@@ -0,0 +1,41 @@
+package com.jurgens.merchantplatform.dto;
+
+public class CategoryResponse {
+
+    private Long id;
+    private String name;
+    private String description;
+    private Long merchantId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getMerchantId() {
+        return merchantId;
+    }
+
+    public void setMerchantId(Long merchantId) {
+        this.merchantId = merchantId;
+    }
+}
--- backend/src/main/java/com/jurgens/merchantplatform/services/CategoryService.java
+++ backend/src/main/java/com/jurgens/merchantplatform/services/CategoryService.java
@@ -1,5 +1,8 @@
 package com.jurgens.merchantplatform.services;
 
+import com.jurgens.merchantplatform.dto.CategoryResponse;
+import com.jurgens.merchantplatform.dto.CreateCategoryRequest;
+import com.jurgens.merchantplatform.dto.UpdateCategoryRequest;
 import com.jurgens.merchantplatform.entities.Category;
 import com.jurgens.merchantplatform.entities.Merchant;
 import com.jurgens.merchantplatform.exceptions.ResourceNotFoundException;
@@ -15,39 +18,87 @@ public class CategoryService {
     private final CategoryRepository categoryRepository;
     private final MerchantRepository merchantRepository;
 
-    public CategoryService(CategoryRepository categoryRepository, MerchantRepository merchantRepository) {
+    public CategoryService(CategoryRepository categoryRepository,
+                           MerchantRepository merchantRepository) {
         this.categoryRepository = categoryRepository;
         this.merchantRepository = merchantRepository;
     }
 
-    public Category createCategory(Long merchantId, Category category) {
+    public CategoryResponse createCategory(
+            Long merchantId,
+            CreateCategoryRequest request) {
+
         Merchant merchant = merchantRepository.findById(merchantId)
-                .orElseThrow(() -> new ResourceNotFoundException("Merchant not found with id: " + merchantId));
+                .orElseThrow(() ->
+                        new ResourceNotFoundException(
+                                "Merchant not found with id: " + merchantId
+                        ));
 
+        Category category = new Category();
+
+        category.setName(request.getName());
+        category.setDescription(request.getDescription());
         category.setMerchant(merchant);
-        return categoryRepository.save(category);
+
+        Category savedCategory = categoryRepository.save(category);
+
+        return convertToResponse(savedCategory);
     }
 
-    public List<Category> getCategoriesByMerchant(Long merchantId) {
-        return categoryRepository.findByMerchantId(merchantId);
+    public List<CategoryResponse> getCategoriesByMerchant(Long merchantId) {
+
+        return categoryRepository.findByMerchantId(merchantId)
+                .stream()
+                .map(this::convertToResponse)
+                .toList();
     }
 
-    public Category getCategoryById(Long id) {
-        return categoryRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Category not found with id: " + id));
+    public CategoryResponse getCategoryById(Long id) {
+
+        Category category = getCategoryEntityById(id);
+
+        return convertToResponse(category);
     }
 
-    public Category updateCategory(Long id, Category categoryDetails) {
-        Category existingCategory = getCategoryById(id);
+    public CategoryResponse updateCategory(
+            Long id,
+            UpdateCategoryRequest request) {
 
-        existingCategory.setName(categoryDetails.getName());
-        existingCategory.setDescription(categoryDetails.getDescription());
+        Category existingCategory = getCategoryEntityById(id);
 
-        return categoryRepository.save(existingCategory);
+        existingCategory.setName(request.getName());
+        existingCategory.setDescription(request.getDescription());
+
+        Category updatedCategory = categoryRepository.save(existingCategory);
+
+        return convertToResponse(updatedCategory);
     }
 
     public void deleteCategory(Long id) {
-        Category category = getCategoryById(id);
+
+        Category category = getCategoryEntityById(id);
+
         categoryRepository.delete(category);
+    }
+
+    private Category getCategoryEntityById(Long id) {
+
+        return categoryRepository.findById(id)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException(
+                                "Category not found with id: " + id
+                        ));
+    }
+
+    private CategoryResponse convertToResponse(Category category) {
+
+        CategoryResponse response = new CategoryResponse();
+
+        response.setId(category.getId());
+        response.setName(category.getName());
+        response.setDescription(category.getDescription());
+        response.setMerchantId(category.getMerchant().getId());
+
+        return response;
     }
 }
